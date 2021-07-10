0c6e7026d4239af64bcb353d06cbbe56163eb717
Scott Sideleau <bigd0g@porkchop.local>
2021-07-10 10:00:57 -0400

    Added Terms of Use to script

--- get-python-src.sh
+++ get-python-src.sh
@@ -1,5 +1,22 @@
 #! /bin/bash
 
+# File:    get-python-src.sh
+# Github:  https://github.com/scottsideleau/get-python-src
+# License: Copyright (C) 2021, Scott R. Sideleau (@scottsideleau)
+#
+#          Licensed under the Apache License, Version 2.0 (the "License");
+#          you may not use this file except in compliance with the License.
+#          A copy of the License is distributed with this source.  Else,
+#          you may obtain a copy of the License at:
+#
+#          https://www.apache.org/licenses/LICENSE-2.0.html
+#
+#          Unless required by applicable law or agreed to in writing, software
+#          distributed under the License is distributed on an "AS IS" BASIS,
+#          WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+#          implied.  See the License for the specific language governing 
+#          permissions and limitations under the License.
+
 WELCOME="
   FETCH (and/or BUILD) source for Custom Python Environment
   ------------------------------------------------------------------------"
